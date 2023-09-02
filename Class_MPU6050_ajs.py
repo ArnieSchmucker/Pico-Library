c575c230ed65b1d225bd72c7e0fd505772676ec7
ArnieSchmucker <arnold_schmucker@yahoo.com>
2023-09-02 14:06:16 -0700

    Update Class_MPU6050_ajs.py

--- Class_MPU6050_ajs.py
+++ Class_MPU6050_ajs.py
@@ -2,34 +2,56 @@ from machine import Pin,PWM,I2C
 from imu import MPU6050
 import time
 import math
-
+        
 class Inertial_Sensor:
-    def __init__(self):
-        self.name = 'Outer Classs'
-        self.sensor = self.Sensor()
+    def __init__(self,chan,sdaPin,sdlPin,freq=400000):
+        self.name = 'MPU6050 Sensor'
+        self.chan = chan
+        self.sdaPin = sdaPin
+        self.sdlPin = sdlPin
+        self.freq = freq
+        self.i2c = I2C(self.chan, sda=Pin(self.sdaPin), scl=Pin(self.sdlPin), freq=self.freq)
+        self.mpu = MPU6050(self.i2c)
         
     def show(self):
         print(self.name)
-        print(self.sensor)
+        print(self.i2c)
+        print(self.mpu)
         
-    class Sensor:
-        def __init__(self):
-            self.name = 'Inner Class'
-            self.i2c = I2C(1, sda=Pin(2), scl=Pin(3), freq=400000)
-            self.mpu = MPU6050(self.i2c)
-            
-        def show(self):
-            print(self.name)
-            print(self.i2c)
-            print(self.mpu)
-            
-        def read_data(self):
-            self.accelX = self.mpu.accel.x
-            self.accelY = self.mpu.accel.y
-            self.accelZ = self.mpu.accel.z
-            
-        def print_data(self):
-            print(self.accelX,self.accelY,self.accelZ)
+    def calibrate(self,cal_ax=0,cal_ay=0,cal_az=0,cal_gx=0,cal_gy=0,cal_gz=0):
+        self.cal_ax=cal_ax
+        self.cal_ay=cal_ay
+        self.cal_az=cal_az
+        self.cal_gx=cal_gx
+        self.cal_gy=cal_gy
+        self.cal_gz=cal_gz
+        
+    def print_cals(self):
+        print(self.cal_ax,self.cal_ay,self.cal_az,self.cal_gx,self.cal_gy,self.cal_gz)
+        
+    def read_data(self):
+        self.accelX = self.mpu.accel.x-self.cal_ax
+        self.accelY = self.mpu.accel.y-self.cal_ay
+        self.accelZ = self.mpu.accel.z-self.cal_az
+        self.gyroX = self.mpu.gyro.x-self.cal_gx
+        self.gyroY = self.mpu.gyro.y-self.cal_gy
+        self.gyroZ = self.mpu.gyro.z-self.cal_gz
+        
+    def print_data(self):
+        print(self.accelX,self.accelY,self.accelZ,self.gyroX,self.gyroY,self.gyroZ)
+        
+    def accel_angles(self):
+        self.angles_AX = math.atan2(self.accelX,self.accelZ)*57.3
+        self.angles_AY=math.atan2(self.accelY,self.accelZ)*57.3
+        
+    def print_accel_data(self):
+        print("inclination X: {:.3f} inclination Y: {:.3f}".format(self.angles_AX,self.angles_AY))
+        
+#     def get_accelA_X(self):
+#         return anglesA[0]
+# 
+#     def get_accelA_Y(self):
+#         return anglesA[1]
         
 # s1 = Inertial_Sensor()
 # s1.show()
